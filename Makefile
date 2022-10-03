dd09913a1fbcaa987307148f5101ef5ae3245b1f
Noah Gift <noah.gift@gmail.com>
2022-10-03 12:33:32 +0000

    build hello world calculator and test

---
+++ Makefile
@@ -0,0 +1,17 @@
+install:
+	pip install --upgrade pip &&\
+		pip install -r requirements.txt
+
+test:
+	echo "Not implemented yet"
+	#python -m pytest -vv test_*.py
+
+format:	
+	black *.py awstools/*.py
+
+lint:
+	pylint --disable=R,C --ignore-patterns=test_.*?py *.py 
+
+refactor: format lint
+
+all: install lint test
---
+++ hello.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python
+
+#build an add function
+def add(a,b):
+    return a+b
+
+#build a subtract function
+def subtract(a,b):
+    return a-b
+
+#build a multiply function
+def multiply(a,b):
+    return a*b
+
+#build a divide function
+def divide(a,b):
+    return a/b
+
+if __name__ == "__main__":
+    print("I'm going use the calculator functions to multiply 5 and 6")
+    x = multiply(5,6)
+    print(x)
